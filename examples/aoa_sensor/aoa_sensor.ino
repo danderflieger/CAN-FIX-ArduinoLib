0ef1acd573723226cb822c862e9dee6c5841a6a0
Dan Der Flieger <dan.der.flieger@gmail.com>
2023-05-10 18:36:39 -0400

    Added a bunch of stuff to update the EEPROM on the AoA node from another node (aoa_sensor_angle_setter) using CAN messages.
    I also attempted to set the AoA Meta data on the FIXGW, but that's not updating. It all compiles and mostly works, apart from
    my messages probably not being formatted correctly. I'm just checking in because I'm out of time today and want a copy of it.

--- examples/aoa_sensor/aoa_sensor.ino
+++ examples/aoa_sensor/aoa_sensor.ino
@@ -12,7 +12,7 @@
 // located in the EEPROM chip - each reading is a Float value, which is 4 bytes long)
 #define EEPROM_ZERO_G_POSITION  0x00
 #define EEPROM_WARN_POSITION    0x05
-#define EEPROM_STALL_POSITION   0x09
+#define EEPROM_STALL_POSITION   0x10
 
 #define META_MIN      0b00010000
 #define META_MAX      0b00100000
@@ -22,10 +22,9 @@
 #define CAN0_INT      2
 MCP_CAN CAN0(10);
 
-CanFix cf(0x77);
+CanFix cf(0x78);
 
 AMS_5600 ams5600;
-
 
 float lastAnglePitch = 0.0;
 unsigned long now;
@@ -34,20 +33,17 @@ unsigned long messageSendInterval = 100;
 float smoothReadings = 0.0;
 int smoothReadingsCount = 0;
 
-
-
 void setup() {
   Serial.println("Begin Setup");
   Serial.begin(115200);
   
-  cf.setDeviceId(0x77);
+  cf.setDeviceId(0x78);
   cf.setModel(0x12345);
   cf.setFwVersion(2);
  
   cf.set_write_callback(can_write_callback);
+  // cf.set_report_callback(report_callback);
 
-  // pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(6, INPUT);
   delay(250);
   Wire.begin();
   
@@ -74,59 +70,105 @@ void setup() {
 
 void loop() {
 
-  float zeroGValue;
-  EEPROM.get(EEPROM_ZERO_G_POSITION, zeroGValue);
+  long unsigned int rxId;
+  unsigned char len = 0;
+  char msgString[128];
+  unsigned char rxBuf[8];
   
+  // First, get the reading from the encoder. This is exactly what the
+  // encoder reads, with no clean-up. If the diametric magnet is 3.2 degrees
+  // from 0 on the encoder, that's what we read here. We subtract 180 degrees 
+  // from that so we have a range of -180 to 180, rather than 0-360
   float rawAnglePitch = convertRawAngleToDegrees(ams5600.getRawAngle()) - 180;
 
-
+  float zeroGValue;
+  EEPROM.get(EEPROM_ZERO_G_POSITION, zeroGValue);
 
   unsigned long now = millis();
   float pitchAngle = rawAnglePitch;
 
+  if (zeroGValue != NAN) {
+    pitchAngle -= zeroGValue;
+  }
+
+  // Next, check to see if there's a message waiting in the CAN buffer. We 
+  // specifically want to find one with the 0x30F identifier, which we will
+  // use as a signal to update important AoA angles (level flight, warning, and stall)
+  if (!digitalRead(CAN0_INT)) {
+    // if there is a CAN message waiting, declare a couple of variables that we 
+    // can use to investigate the message
+    
+
+    // read the message and assign parts to the variables above
+    CAN0.readMsgBuf(&rxId, &len, rxBuf);
+    if ((rxId & 0x80000000) != 0x80000000) {
+      Serial.println(rxId, HEX);
+      
+      // Check to see if the message id is 0x30F, which is undefined in the
+      // CANFIX spec - e.g., we can use it for other things like, say, 
+      // telling our AoA sensor to set some data fields and write the
+      // values to EEPROM so the settings persist after reboot
+      if (rxId == 0x4000030F) {
+        // Create a CanFixFrame object that we can parse from the variables above
+        CanFixFrame frame;
+        frame.id = rxId;
+        frame.length = len;
+        memcpy(frame.data, rxBuf, 8);
+        cf.exec(frame);
+
+        // updateAngleValues(rxBuf[3], rawAnglePitch);
+
+        // Depening on the button pressed on the setter device,
+        // set some values on this device
+        
+        int buttonId = rxBuf[3]; // frame.data[3];
+
+        Serial.print("Button pressed: ");
+        Serial.print(buttonId);
+
+        if (buttonId == 0x00) {  
+          writeEEPROM(EEPROM_ZERO_G_POSITION , rawAnglePitch);
+          setMeta(META_ZERO_G, pitchAngle);
+        } else if (buttonId == 0x01) {
+          writeEEPROM(EEPROM_WARN_POSITION, pitchAngle);
+          // setMeta(META_WARN, pitchAngle);
+        } else if (buttonId == 0x02) {
+          writeEEPROM(EEPROM_STALL_POSITION, pitchAngle);
+          // setMeta(META_STALL, pitchAngle);
+        } else {
+          // Do nothing, not expecting whatever data we received
+        }
+        
+        // if (frame.id == 0x30F) {
+        //   writeEEPROM(rxBuf[3], rawAnglePitch)
+        // }
+      }
+    
+    } 
+
+    
+  }
+
 
   
   if(now - lastMessageTimerMillis < messageSendInterval) {
-
-
 
     smoothReadings += pitchAngle;
     smoothReadingsCount++;
 
   } else {
 
-    Serial.print("rawAnglePitch: ");
-    Serial.print(rawAnglePitch);
-    Serial.print("\tzeroGValue: ");
-    Serial.println(zeroGValue);
-
-    // Serial.print("\tnew rawAnglePitch: ");
-    // Serial.println(rawAnglePitch);
-
 
     pitchAngle = smoothReadings / smoothReadingsCount;
 
-    if (zeroGValue != NAN) {
-      pitchAngle -= zeroGValue;
-    }    
-
-    if (digitalRead(6) == HIGH){
-      
-      if (rawAnglePitch == zeroGValue) {
-        Serial.println("Values already match");
-      } else {
-        Serial.print("Updating Zero G Value to: ");
-        EEPROM.put(EEPROM_ZERO_G_POSITION, rawAnglePitch);
-        Serial.println(rawAnglePitch);
-      }    
-      
-    } else {
-      
-      // Serial.print("EEPROM Zero G Reading: ");
-      // Serial.println(zeroGValue);
-    }
 
     sendAoAPitchAngle(pitchAngle);
+
+    // // Check to see if this is an extended CAN Identifier
+    // // (CANFIX does not use extended, so we'll ignore those)
+    // if(!digitalRead(CAN0_INT)) {
+      
+    // }
 
     lastAnglePitch = pitchAngle;
     lastMessageTimerMillis = now;
@@ -170,7 +212,6 @@ void sendAoAPitchAngle(float angle) {
 
     CFParameter pAngleOfAttack;
     pAngleOfAttack.type = 0x182; // CAN-FIX Angle of Attack Parameter
-    // pAngleOfAttack.type = 0x180; // CAN-FIX Pitch Angle Parameter (testing)
     pAngleOfAttack.index = 0x00;
     pAngleOfAttack.fcb = 0x00;
     pAngleOfAttack.data[0] = x;
@@ -192,20 +233,62 @@ void sendAoAPitchAngle(float angle) {
   for that meta type. Then it pushes the update over the CAN bus for 
   use on the EFIS screen
 *********************************************************************/
-void setMeta(byte metaType, float data) {
+void setMeta(byte metaType, float angle) {
 
-  CFParameter p;
-  int x;
+  CFParameter pAoAMeta;
+  signed int x = angle * 100;
 
-  p.type = 0x182; // CAN-FIX Angle of Attack Parameter
-  p.index = 0x00;
-  p.fcb = metaType;
-  x = data*100;
-  p.data[0] = x;
-  p.data[1] = x>>8;
-  p.length = 2;
+  pAoAMeta.type = 0x182; // CAN-FIX Angle of Attack Parameter
+  pAoAMeta.index = 0x00;
+  pAoAMeta.fcb = metaType;
+  pAoAMeta.data[0] = x;
+  pAoAMeta.data[1] = x>>8;
+  pAoAMeta.data[3] = x>>16;
+  pAoAMeta.data[4] = x>>24;
+  pAoAMeta.length = 7;
+  cf.sendParam(pAoAMeta);
+  
 
-  cf.sendParam(p);
+}
+
+void updateAngleValues(int buttonId, float angle) {
+  
+  // Depening on the button pressed on the setter device,
+  // set some values on this device
+  Serial.print("Button pressed: ");
+  Serial.print(buttonId);
+
+  if (buttonId == 0x00) {  
+    writeEEPROM(EEPROM_ZERO_G_POSITION , angle);
+    setMeta(META_ZERO_G, angle);
+  } else if (buttonId == 0x01) {
+    writeEEPROM(EEPROM_WARN_POSITION, angle);
+  } else if (buttonId == 0x02) {
+    writeEEPROM(EEPROM_STALL_POSITION, angle);
+  } else {
+    // Do nothing, not expecting whatever data we received
+  }
+}
+
+void writeEEPROM(int eepromPosition, float angle) {
+
+  float currentAngle;
+  EEPROM.get(eepromPosition, currentAngle);
+
+  Serial.print("\ncurrentAngle: ");
+  Serial.print(currentAngle);
+  Serial.print("\tangle: ");
+  Serial.println(angle);
+
+  if (currentAngle == angle) {
+    Serial.println("Values already match");
+  } else {
+    EEPROM.put(eepromPosition, angle);
+    Serial.print("Updating angle: ");
+    Serial.print(eepromPosition);
+    Serial.print(" to: ");
+    Serial.println(angle);
+  }
 
 }
 
